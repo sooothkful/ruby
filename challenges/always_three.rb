81aa69b384b30d999bf535ac34e8c7fe38323608
sooot <sooothkful@gmail.com>
2019-12-16 20:04:40 -0600

    methods!

--- challenges/always_three.rb
+++ challenges/always_three.rb
@@ -1,5 +1,12 @@
+
+
 puts "Give me a number"
-final_number = 1555
-final_number = gets.to_i
-final_number = (((final_number + 5)*2)-4)/2
-puts "#{final_number}"
+
+def my_math_method(num1)
+num1 = gets.to_i
+num1 = (((num1+ 5)*2)-4)/2
+puts "Always" + " #{num1}"
+end
+
+my_math_method(1555)
+my_math_method(123546)
