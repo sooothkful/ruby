7ab89a4ae0370c045c005389d777b6b5cd9f5cab
sooot <sooothkful@gmail.com>
2019-12-22 16:12:15 -0600

    yay fizz_buzz

--- challenges/fizz_buzz.rb
+++ challenges/fizz_buzz.rb
@@ -1,17 +1,23 @@
-number = '1'
+number = '0'
 puts number
 number = number.to_i
 
+
 while (number < 100)
 number = number.to_i + 1
-puts number
 number_zero_div_five = number % 5
 number_zero_div_three = number % 3
 buzz = "buzz"
 fizz = "fizz"
- if (number_zero_div_five == 0)
+fizzbuzz = "fizzbuzz"
+if (number_zero_div_five == 0 && number_zero_div_three == 0)
+puts fizzbuzz
+elsif(number_zero_div_five == 0)
   puts buzz
 elsif (number_zero_div_three == 0)
-   puts fizz
- end
+ puts fizz
+elsif
+  puts number
+
+end
 end
